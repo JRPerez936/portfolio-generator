922a60495619f394631759c69fdf0cf957079e2d
Justin Perez <perezjrp936@gmail.com>
2021-08-22 13:11:40 -0400

    corrected code with snapshot

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,7 +19,7 @@ const writeFile = fileContent => {
   };
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-      fs.copyFile('./dist/style.css', fileContent, err => {
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
         // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
         if (err) {
           reject(err);
@@ -30,7 +30,7 @@ const copyFile = () => {
         // if everything went well, resolve the Promise and send the successful data to the `.then()` method
         resolve({
           ok: true,
-          message: 'File copied!'
+          message: 'Stylesheet created!'
         });
       });
     });
